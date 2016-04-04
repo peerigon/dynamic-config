e4ce97b4388324773c0a1219ddb657debd95a6c1
meaku <michael.jaser@peerigon.com>
2016-04-03 18:42:18 -0700

    Add envName option which defaults to "env" - add tests - update dependencies

---
+++ test/index.test.js
@@ -0,0 +1,36 @@
+"use strict";
+
+import test from "ava";
+import { expect } from "chai";
+import loadConfig from "../lib/index";
+import a from "./fixtures/configs/a/config";
+import b from "./fixtures/configs/b/config";
+
+function config() {
+    return loadConfig(__dirname + "/fixtures/configs/", "config.js");
+}
+
+test("should accept a defaultEnv", () => {
+    loadConfig.options.defaultEnv = "a";
+
+    const conf = config();
+    expect(conf).to.eql(a);
+});
+
+test("should load the config according to the env", () => {
+    loadConfig.options.defaultEnv = "a";
+
+    process.env.env = "b";
+
+    expect(config()).to.eql(b);
+});
+
+
+test("should accept a custom env name via options.envName", () => {
+    loadConfig.options.defaultEnv = "a";
+    loadConfig.options.envName = "wookie";
+
+    process.env.wookie = "b";
+
+    expect(config()).to.eql(b);
+});
