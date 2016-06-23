1b6633cd326dbf8c09491f238183929a9cd4385e
Johannes Ewald <johannes.ewald@peerigon.com>
2016-06-23 14:33:36 +0200

    Improve tests

--- test/index.test.js
+++ test/index.test.js
@@ -7,46 +7,72 @@ import DynamicConfig from "../lib/index";
 import a from "./fixtures/configs/a/config";
 import b from "./fixtures/configs/b/config";
 
-const dynamicConfig = new DynamicConfig();
+const envPropertiesToClear = [
+    "env",
+    "wookie"
+];
+const loadArgs = [path.resolve(__dirname, "fixtures", "configs"), "config.js"];
 
-function config() {
-    return dynamicConfig.load(path.resolve(__dirname, "fixtures", "configs"), "config.js");
-}
+test.afterEach(() => {
+    envPropertiesToClear.forEach(prop => delete process.env[prop]);
+});
 
-test("should accept a defaultEnv", () => {
+test("Should accept a defaultEnv", () => {
+    const dynamicConfig = new DynamicConfig({
+        defaultEnv: "a"
+    });
+
     dynamicConfig.options.defaultEnv = "a";
 
-    const conf = config();
+    const conf = dynamicConfig.load(...loadArgs);
 
     expect(conf).to.eql(a);
 });
 
-test("should load the config according to the env", () => {
-    dynamicConfig.options.defaultEnv = "a";
+test("Should load the config according to the env", () => {
+    const dynamicConfig = new DynamicConfig();
 
     process.env.env = "b";
 
-    expect(config()).to.eql(b);
+    expect(dynamicConfig.load(...loadArgs)).to.eql(b);
 });
 
-test("should accept a custom env name via options.envName", () => {
-    dynamicConfig.options.defaultEnv = "a";
-    dynamicConfig.options.envName = "wookie";
+test("Should accept a custom env name via options.envName", () => {
+    const dynamicConfig = new DynamicConfig({
+        envName: "wookie"
+    });
 
     process.env.wookie = "b";
 
-    expect(config()).to.eql(b);
+    expect(dynamicConfig.load(...loadArgs)).to.eql(b);
 });
 
-test("should preserve stack traces", () => {
+test("Should preserve stack traces", () => {
+    const dynamicConfig = new DynamicConfig({
+        defaultEnv: "withError"
+    });
     let err;
 
-    dynamicConfig.options.defaultEnv = "withError";
     try {
-        config();
+        dynamicConfig.load(...loadArgs);
     } catch (e) {
         err = e;
     }
     expect(err.stack).to.contain("Cannot find module './does-not-exist'");
     expect(err.stack).to.contain("fixtures/configs/withError/config.js:1:1");
+});
+
+test("Should use the provided log function", () => {
+    const dynamicConfig = new DynamicConfig({
+        defaultEnv: "a",
+        log(str) {
+            hasBeenCalled = true;
+            expect(str).to.contain(path.resolve(__dirname, "fixtures", "configs", "a", "config.js"));
+            expect(this).to.equal(console, "Has called options.log on console");
+        }
+    });
+    let hasBeenCalled = false;
+
+    dynamicConfig.load(...loadArgs);
+    expect(hasBeenCalled).to.equal(true, "Has called options.log");
 });
