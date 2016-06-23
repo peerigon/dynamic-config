20f069653cfc7d4d3dfcd24c9bec62413785af96
Johannes Ewald <johannes.ewald@peerigon.com>
2016-06-23 15:16:35 +0200

    Remove outdated examples
    They are all in the README.md

--- example/plugins/extend/config/develop/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    name: "superApp",
-    port: 9000
-};
--- example/simple/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var loadConfig = require("../../lib/index.js");
-
-loadConfig.options.defaultEnv = "develop";
-
-module.exports = loadConfig(__dirname, "config.js");
