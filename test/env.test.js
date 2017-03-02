bb69d78bad9987e6a17f1ff4b2cbb99fb1ed4cb8
meaku <michael.jaser@peerigon.com>
2017-03-02 14:45:38 +0100

    Add test for env plugin

--- test/env.test.js
+++ test/env.test.js
@@ -1,8 +1,86 @@
 "use strict";
 
+import path from "path";
 import test from "ava";
+import { expect } from "chai";
 import envPlugin from "../plugins/extend/env"; // eslint-disable-line no-unused-vars
+import DynamicConfig from "../lib/index";
+import develop from "./fixtures/configs/develop/config";
 
-test("Should read config keys from process.env", () => {
-    // Missing tests
+const envPropertiesToClear = [
+    "env",
+    "whereAmI",
+    "server:host"
+];
+
+test.afterEach(() => {
+    envPropertiesToClear.forEach(prop => delete process.env[prop]);
+});
+
+const loadArgs = [path.resolve(__dirname, "fixtures", "configs"), "config.js"];
+
+test("Should merge top level configuration options passed via env (export whereAmI=envExtended)", () => {
+    const dynamicConfig = new DynamicConfig({});
+
+    dynamicConfig.use(envPlugin);
+
+    process.env.whereAmI = "envExtended";
+
+    const conf = dynamicConfig.load(...loadArgs);
+
+    expect(conf.whereAmI).to.eql(process.env.whereAmI);
+});
+
+test("Should not merge whitelisted env vars (export whereAmI=envExtended)", () => {
+    const dynamicConfig = new DynamicConfig({
+        whitelist: ["whereAmI"]
+    });
+
+    dynamicConfig.use(envPlugin);
+
+    process.env.whereAmI = "envExtended";
+
+    const conf = dynamicConfig.load(...loadArgs);
+
+    expect(conf.whereAmI).to.eql(develop.whereAmI);
+});
+
+test("Should merge nested configuration options passed via env (export server:host=npm.com)", () => {
+    const dynamicConfig = new DynamicConfig({});
+
+    dynamicConfig.use(envPlugin);
+
+    process.env["server:host"] = "npm.com";
+
+    const conf = dynamicConfig.load(...loadArgs);
+
+    expect(conf.server.host).to.eql(process.env["server:host"]);
+});
+
+test("Should merge nexted configuration using a custom seperator via env (export server--host=npm.com)", () => {
+    const dynamicConfig = new DynamicConfig({
+        seperator: "--"
+    });
+
+    dynamicConfig.use(envPlugin);
+
+    process.env["server--host"] = "npm.com";
+
+    const conf = dynamicConfig.load(...loadArgs);
+
+    expect(conf.server.host).to.eql(process.env["server--host"]);
+});
+
+test("Should merge nexted configuration using a custom seperator via env (export server--host=npm.com)", () => {
+    const dynamicConfig = new DynamicConfig({
+        seperator: "--"
+    });
+
+    dynamicConfig.use(envPlugin);
+
+    process.env["server--host"] = "npm.com";
+
+    const conf = dynamicConfig.load(...loadArgs);
+
+    expect(conf.server.host).to.eql(process.env["server--host"]);
 });
---
+++ test/fixtures/configs/develop/config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+    whereAmI: "develop",
+    server: {
+        host: "localhost",
+        port: 3000
+    },
+    longStackTraces: true
+};
